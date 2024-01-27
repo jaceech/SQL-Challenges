9ecbb74cb6f36dd7241b45ca80a0e79439f4de51
jaceech <156471864+jaceech@users.noreply.github.com>
2024-01-26 18:36:37 -0700

    Update all.sql

--- Hard/all.sql
+++ Hard/all.sql
@@ -54,8 +54,8 @@ from patients p join admissions a on p.patient_id = a.patient_id
 Each admission costs $50 for patients without insurance, and $10 for patients with insurance. 
 All patients with an even patient_id have insurance.
 
-Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. 
-Add up the admission_total cost for each has_insurance group.
+Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group.
+    
 SELECT 
 CASE WHEN patient_id % 2 = 0 Then 
     'Yes'
